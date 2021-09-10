15884b601123a51234b69fe8b3f54a14779d89b5
YU ZHE-SIMTech <huangyuzhe2019@gmail.com>
2021-09-10 16:10:57 +0800

    Add "website" Column

---
+++ fetchGet.js
@@ -0,0 +1,25 @@
+fetch("https://jsonplaceholder.typicode.com/users").then(
+    res => {
+        res.json().then(
+            data => {
+                console.log(data);
+
+                if(data.length > 0) {
+                    var temp = "";
+                    var index = 1;
+                    data.forEach((itemData) => {
+                        temp += "<tr>";
+                        temp += "<th scope='row'>" + index +"</th>";
+                        temp += "<td>" + itemData.name + "</td>";
+                        temp += "<td>" + itemData.username + "</td>";
+                        temp += "<td>" + itemData.email + "</td>";
+                        temp += "<td>" + itemData.website + "</td>";
+                        temp += "</tr>";
+                        index++;
+                    });
+                    document.getElementById('data').innerHTML = temp;
+                }
+            }
+        )
+    }
+)
